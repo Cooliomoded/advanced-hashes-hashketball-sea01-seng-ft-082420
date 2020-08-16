3a9bace2c011bd465cb436e9ecf2900b0ddeae16
Davis Brown <cooliomoded@gmail.com>
2020-08-16 19:54:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -165,7 +165,7 @@ end
 def player_numbers(q_player)
   game_hash.each do |team, team_hashes|
     team_hashes[:players].each do |player|
-      return player[:number]
+      player[:number].sort
       binding.pry
     end
   end
