1353e8a257636ff8dbea15347c97213945519e65
Davis Brown <cooliomoded@gmail.com>
2020-08-16 18:30:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -132,7 +132,7 @@ def num_points_scored(player_q)
   game_hash.each do |team, team_hashes|
     team_hashes[:players].each do |position|
       if position[:player_name] == player_q
-        puts player_q[:points]
+        puts position[:points]
       end
     end
   end
